53472f3e52a95ba78e84fa6fd89bf35a889e2a1b
Sayeed613 <sayeedahmed90082@gmail.com>
2025-01-21 16:36:46 +0530

    done

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 function connectToDb() {
     mongoose.connect(process.env.MONGODB_URL
     ).then(() => {
-        console.log('Connected to DB');
+        console.log('Connected to Data base ');
     }).catch(err => console.log(err));
 }
 
